6f7c234d2817ba8671d4a222699c898f5fc5ef3b
Salah <91811442+TechNerd-019@users.noreply.github.com>
2024-10-02 13:23:41 -0400

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,32 +1,26 @@
-# factorial.py 
-
+# factorial.py
+"""Factorial.py"""
 import time
 
 final_list = []
 
-def factorial(n):   
-	time.sleep(.1)
-
-	factorial = 1   
-
-	for i in range (1,n+1):
-
-		factorial = factorial * i
-
-	return factorial     
+def factorial(n):
+    """Find Factorial"""
+    time.sleep(.1)
+    factorial_num = 1
+    for i in range (1,n+1):
+        factorial_num = factorial_num * i
+    return factorial_num
 
 def sum_factorial():
-
-	for i in range(50):
-
-		final_list.append(factorial(i))
-
-	result=sum(final_list)
-
-	print("Final SUM = {}".format(result))
-
-	return result
+    """Sum Factorial"""
+    for i in range(50):
+        final_list.append(factorial(i))
+    result=sum(final_list)
+    print(f"FINAL_SUM = {result}")
+    return result
 
 if __name__ == "__main__":
-
     sum_factorial()
+
+FINAL_SUM = 620960027832821612639424806694551108812720525606160920420940314
